c4b347ba0d3d15a6e56a0826416695585249d64a
murali <muralikrishna2408@gmail.com>
2024-06-27 10:44:10 +0530

    my first commit

---
+++ python_practies/pro1.py
@@ -0,0 +1,10 @@
+num=["murali","krishna","sudeer","mahesh","rajesh"]
+num.append("suresh")
+print(num)
+values=[2,5,6,2,8,3,9]
+values.append([2,2,2])
+
+values.extend([3,34,21])
+print(values)
+values.remove(5)
+print(values)
