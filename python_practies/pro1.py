781796c76f517b94175dd8fd3d47a3f779e9da7a
murali <muralikrishna2408@gmail.com>
2024-06-28 09:40:29 +0530

    my third commit

--- python_practies/pro1.py
+++ python_practies/pro1.py
@@ -2,9 +2,5 @@ num=["murali","krishna","sudeer","mahesh","rajesh"]
 num.append("suresh")
 print(num)
 values=[2,5,6,2,8,3,9]
-values.append([2,2,2])
-
-values.extend([3,34,21])
-print(values)
-values.remove(6)
+values.extend([3,8,1])
 print(values)
